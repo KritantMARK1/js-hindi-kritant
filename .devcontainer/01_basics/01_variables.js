1419db3a9bf009a299081160b5de2c4b4dbd22a4
KritantMARK1 <163982221+KritantMARK1@users.noreply.github.com>
2024-11-09 14:54:00 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const applicationName = "Kritant"
+
+/*never use var again in your whole life for
+ the issue of block scope 
+ and functional scope*/
+var accountpassword = "12345"
+accountpassword="9382129"
+var accountpassword = "11122223333444"
+accountpassword = "69696969696969"
+let companyname = "Prince"
+companyname = "Snehil"
+console.table([applicationName,accountpassword,companyname]);
