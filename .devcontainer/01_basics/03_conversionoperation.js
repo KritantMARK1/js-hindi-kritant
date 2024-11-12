269d28784c017fa0ea32e7a23f916d8d8c1dea7a
KritantMARK1 <163982221+KritantMARK1@users.noreply.github.com>
2024-11-12 08:59:03 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+console.log(typeof (score));
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+
+let push = null
+console.log(typeof null);
+let value = Number(null)
+console.log(value);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true => 1
+//false => 0
+
+let isloggedin = "kritaant"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// 1 - true
+// 0 - false
+//empty string - false
+//"kritant" - true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+
+
