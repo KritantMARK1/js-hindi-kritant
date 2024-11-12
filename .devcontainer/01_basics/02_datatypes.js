caf3bbf4b94db1770cda0e34ef0f9be79979b643
KritantMARK1 <163982221+KritantMARK1@users.noreply.github.com>
2024-11-12 08:10:25 +0000

    datatypes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -1,4 +1,26 @@
+"use strict";
 console.log( 3 + 3 )
 console.log("hello")
+let state = null
+console.log(state-1);
+
+
+let name = ("KRITANT")
+console.log(name);
+
+//number => 2 to the power 53
+//bigint
+//string => "" 
+//boolean => true or false
+//null => standalone value 
+//undefined =>
+//symbol => unique
+//object
+console.log(typeof undefined);
+
+//if we take out the type of "null" it will be come as object 
+
+
+
 
 
