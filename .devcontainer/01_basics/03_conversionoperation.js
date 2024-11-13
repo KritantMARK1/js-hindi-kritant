ccd43c7a86fd5319eab3a6bda8bd9835d290f366
KritantMARK1 <163982221+KritantMARK1@users.noreply.github.com>
2024-11-13 17:28:59 +0000

    conversion

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -1,14 +1,14 @@
 let score = "33"
-console.log(typeof (score));
+// console.log(typeof (score));
 let valueinNumber = Number(score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
 
 
 let push = null
-console.log(typeof null);
+// console.log(typeof null);
 let value = Number(null)
-console.log(value);
+// console.log(value);
 
 //"33"=> 33
 //"33abc"=> NaN
@@ -17,7 +17,7 @@ console.log(value);
 
 let isloggedin = "kritaant"
 let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+// console.log(booleanisloggedin);
 
 // 1 - true
 // 0 - false
@@ -26,6 +26,35 @@ console.log(booleanisloggedin);
 
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber);
+//console.log(stringnumber);
+
+// ***************************Operations***********************//
+
+let nags = 3
+let negnags = -nags
+// console.log(negnags);
+
+// console.log(2**55);
+// console.log(2*55);
+// console.log(2/55);
+// console.log(2%55);
+// console.log(2+55);
+// console.log(2-55);
+
+let str1 = "hello "
+let str2 = "Kritant"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(2+2+"1");
+// console.log("2"+"2"+"1");
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
 
 
